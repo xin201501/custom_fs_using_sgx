74c9230e43359670765cdc452962eaaf444a7e18
xin201501 <2386772327@qq.com>
2023-03-16 18:10:44 +0800

    初步实现enclave内逻辑

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -14,28 +14,106 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License..
-
 use sgx_types::{error::SgxStatus, types::EnclaveId};
-use sgx_urts::enclave::SgxEnclave;
 extern "C" {
-    fn sample(eid: EnclaveId, ret: *mut SgxStatus);
+    fn sample(eid: EnclaveId, ret: *mut SgxStatus) -> SgxStatus;
+    fn aes_xts_128bit_128bit_KEK_encryption(
+        eid: EnclaveId,
+        kek: &[u8; 16],
+        plaintext: *const u8,
+        plaintext_len: usize,
+        sector_size: usize,
+        sector_index: u64,
+        ciphertext: *mut u8,
+    ) -> SgxStatus;
+    fn aes_xts_128bit_128bit_KEK_decryption(
+        eid: EnclaveId,
+        kek: &[u8; 16],
+        ciphertext: *const u8,
+        ciphertext_len: usize,
+        sector_size: usize,
+        sector_index: u64,
+        plaintext: *mut u8,
+    ) -> SgxStatus;
+    fn test_xts_mode1(eid: EnclaveId, retval: *mut SgxStatus) -> SgxStatus;
 }
 
-fn test_sample() -> anyhow::Result<()> {
-    // create an enclave
-    let enclave = SgxEnclave::create("../bin/enclave.signed.so", true)?;
-    let mut sgx_status = SgxStatus::BadStatus;
-    unsafe { sample(enclave.eid(), &mut sgx_status) };
-    // match `sgx_status`
-    match sgx_status {
-        SgxStatus::Success => Ok(()),
-        _ => Err(anyhow::anyhow!(
-            "enclave run failed with status: {:?}",
-            sgx_status
-        )),
+#[cfg(test)]
+mod tests {
+    use sgx_urts::enclave::SgxEnclave;
+
+    use super::*;
+    /// Test AES-XTS 128-bit KEK encryption
+    /// Reference:https://docs.rs/xts-mode/0.4.0/xts_mode/index.html
+    #[test]
+    fn test_aes_xts_128bit_128bit_kek_encryption_and_decryption() -> anyhow::Result<()> {
+        // create an enclave
+        let enclave = SgxEnclave::create("../bin/enclave.signed.so", true)?;
+        let mut plaintext = [5; 0x400];
+        let mut ciphertext = [0u8; 1024];
+
+        let sector_size = 0x200;
+        let first_sector_index = 0;
+        let mut sgx_status = unsafe {
+            aes_xts_128bit_128bit_KEK_encryption(
+                enclave.eid(),
+                &[0u8; 16],
+                plaintext.as_ptr(),
+                plaintext.len(),
+                sector_size,
+                first_sector_index,
+                ciphertext.as_mut_ptr(),
+            )
+        };
+
+        match sgx_status {
+            SgxStatus::Success => (),
+            _ => {
+                return Err(anyhow::anyhow!(
+                    "enclave run failed with status: {:?}",
+                    sgx_status
+                ))
+            }
+        };
+
+        sgx_status = unsafe {
+            aes_xts_128bit_128bit_KEK_decryption(
+                enclave.eid(),
+                &[0u8; 16],
+                ciphertext.as_ptr(),
+                ciphertext.len(),
+                sector_size,
+                first_sector_index,
+                plaintext.as_mut_ptr(),
+            )
+        };
+
+        match sgx_status {
+            SgxStatus::Success => (),
+            _ => {
+                return Err(anyhow::anyhow!(
+                    "enclave run failed with status: {:?}",
+                    sgx_status
+                ))
+            }
+        };
+
+        assert_eq!(plaintext, [5; 0x400]);
+        Ok(())
     }
-}
-#[test]
-fn test() {
-    test_sample().unwrap();
+    #[test]
+    fn test_sample() -> anyhow::Result<()> {
+        // create an enclave
+        let enclave = SgxEnclave::create("../bin/enclave.signed.so", true)?;
+        let mut sgx_status = SgxStatus::BadStatus;
+        unsafe { test_xts_mode1(enclave.eid(), &mut sgx_status) };
+        // match `sgx_status`
+        match sgx_status {
+            SgxStatus::Success => Ok(()),
+            _ => Err(anyhow::anyhow!(
+                "enclave run failed with status: {:?}",
+                sgx_status
+            )),
+        }
+    }
 }
