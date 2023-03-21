a4396631b05a1fcb3a23bd7aad27d389c50c8081
xin201501 <2386772327@qq.com>
2023-03-21 21:53:54 +0800

    优化复制两次KEK生成key wrap算法输入的逻辑，尚未测试

--- enclave/src/key_management/wrap_key.rs
+++ enclave/src/key_management/wrap_key.rs
@@ -5,20 +5,29 @@ use arx_kw::{
 };
 pub(crate) fn wrap_key_into_vec(kek: &[u8; 16], key: &[u8]) -> anyhow::Result<Vec<u8>> {
     let mut kek_expand = [0u8; 32];
-    kek_expand.copy_from_slice(&kek.repeat(2));
+    let (first_half, second_half) = kek_expand.split_at_mut(16);
+    first_half.copy_from_slice(kek);
+    second_half.copy_from_slice(kek);
+
     let wrapped_key = GX::encrypt_blob(&kek_expand, &key).unwrap();
     Ok(wrapped_key)
 }
 pub(crate) fn wrap_key(kek: &[u8; 16], key: &[u8], wrapped_key: &mut [u8]) -> anyhow::Result<()> {
     let mut kek_expand = [0u8; 32];
-    kek_expand.copy_from_slice(&kek.repeat(2));
+    let (first_half, second_half) = kek_expand.split_at_mut(16);
+    first_half.copy_from_slice(kek);
+    second_half.copy_from_slice(kek);
+
     let result = GX::encrypt_blob(&kek_expand, &key).unwrap();
     wrapped_key.copy_from_slice(&result);
     Ok(())
 }
 pub(crate) fn unwrap_key_into_vec(kek: &[u8; 16], wrapped_key: &[u8]) -> anyhow::Result<Vec<u8>> {
     let mut kek_expand = [0u8; 32];
-    kek_expand.copy_from_slice(&kek.repeat(2));
+    let (first_half, second_half) = kek_expand.split_at_mut(16);
+    first_half.copy_from_slice(kek);
+    second_half.copy_from_slice(kek);
+
     let unwrapped_key = GX::decrypt_blob(&kek_expand, &wrapped_key).unwrap();
     Ok(unwrapped_key)
 }
@@ -28,7 +37,10 @@ pub(crate) fn unwrap_key(
     unwrapped_key: &mut [u8],
 ) -> anyhow::Result<()> {
     let mut kek_expand = [0u8; 32];
-    kek_expand.copy_from_slice(&kek.repeat(2));
+    let (first_half, second_half) = kek_expand.split_at_mut(16);
+    first_half.copy_from_slice(kek);
+    second_half.copy_from_slice(kek);
+
     let result = GX::decrypt_blob(&kek_expand, &wrapped_key).unwrap();
     unwrapped_key.copy_from_slice(&result);
     Ok(())
